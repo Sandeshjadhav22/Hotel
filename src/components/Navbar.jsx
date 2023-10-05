95420354d8cc08954363e7995532374bc6dd26c4
sandesh jadhav <sandeshjadhav2002@gmail.com>
2023-10-05 09:34:30 +0530

    Mobile Drawer functionality created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState} from "react";
 import { AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import {BsFillCartFill, BsFillSaveFill} from 'react-icons/bs';
 import {TbTruckDelivery} from 'react-icons/tb'
@@ -6,11 +6,13 @@ import {FaUserFriends,FaWallet} from 'react-icons/fa'
 import {MdFavorite,MdHelp} from 'react-icons/md'
 
 const Navbar = () => {
+ const [nav,setNav] = useState(false)
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
         {/*Left side  */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 ">
@@ -34,11 +36,11 @@ const Navbar = () => {
       </button>
      {/*Mobile Menu */}
      {/*Overlay*/}
-  
-      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+       {nav ? <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>: ''}
+      
      {/*Side drawermenu */}
-     <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
-     <AiOutlineClose size={30} className="absolute  right-4 top-4 cursor-pointer"/>
+     <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300' }>
+     <AiOutlineClose onClick={() => setNav(!nav)} size={30} className="absolute  right-4 top-4 cursor-pointer"/>
      <h2 className="text-2xl p-4">Best <span className="font-bold">Eats</span></h2>
      <nav>
       <ul className="flex flex-col p-4 text-gray-800">
