ab36068c4a921de0db07f7e9e7bdeb9047de9715
sandesh jadhav <sandeshjadhav2002@gmail.com>
2023-10-05 21:49:37 +0530

    Filter logic Done & Project completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import HeadLineCards from "./components/HeadLineCards";
 import Food from "./components/Food";
+import Category from "./components/Category";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
      <Hero/>
      <HeadLineCards/>
      <Food/>
+     <Category/>
     </div>
   );
 }
